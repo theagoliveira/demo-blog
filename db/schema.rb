a5a5abec9498ce9004ae8304eb0b67765918c73f
Thiago Cavalcante <thiago.kun@gmail.com>
2019-08-01 18:28:36 -0300

    Generate controller and model for comments and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_004437) do
+ActiveRecord::Schema.define(version: 2019_08_01_212701) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
